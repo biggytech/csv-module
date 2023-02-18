af594ab83438fc8508d026c44994530f34f3b06a
Neli Harbuzava <ineliharbuzava@gmail.com>
2023-02-18 13:43:15 +0300

    Setup eslint and prettier

--- src/classes/Dialect.ts
+++ src/classes/Dialect.ts
@@ -1,7 +1,26 @@
-import { IDialect } from "../types";
+import { IDialect, EQuoting } from '../types';
 
 // TODO: implement
-// @ts-ignore
 export class Dialect implements IDialect {
+  delimiter: string;
+  doublequote: boolean;
+  escapechar: string;
+  lineterminator: string;
+  quotechar: string;
+  quoting: EQuoting;
+  skipinitialspace: boolean;
+  strict: boolean;
 
+  constructor() {
+    // TODO: implement
+    // this is just a mock to ignore TS+ Eslint errors for now
+    this.delimiter = ',';
+    this.doublequote = false;
+    this.escapechar = '';
+    this.lineterminator = '\n';
+    this.quotechar = '';
+    this.quoting = EQuoting.QUOTE_ALL;
+    this.skipinitialspace = false;
+    this.strict = false;
+  }
 }
